66fa0111e56057fe14712ae5549ea19a3d81f1ac
Aidan-Davis <98002205+Aidan-Davis@users.noreply.github.com>
2022-07-26 22:02:53 -0400

    Add files via upload
    Removed String uniqueID and String username from user. We haven't used this.

--- User.java
+++ User.java
@@ -2,13 +2,9 @@ import java.util.ArrayList;
 
 public class User {
     private String name; // name of user
-    private String email;
-    private String studentID;
 
-    public User(String name, String email) {
+    public User(String name) {
         this.name = name;
-        this.email = email;
-        this.studentID = name + email;
     }
 
     // Accessors
@@ -16,26 +12,13 @@ public class User {
         return name;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getStudentID() {
-        return studentID;
-    }
-
     // Mutators
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     // toString
     public String toString() {
-        return "Name: " + this.getName() + "/n" +
-                "Email: " + this.getEmail();
+        return "Name: " + getName();
     }
 }
