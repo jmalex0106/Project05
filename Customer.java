f5a9954591cb13d891aac5f5ea018fa67e0a675d
jmalex0106 <42811641+jmalex0106@users.noreply.github.com>
2022-07-29 17:10:02 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -23,7 +23,7 @@ public class Customer {
         return approvedRequest;
     }
 
-    public void remakeCustomerFromFile() {
+    public boolean remakeCustomerFromFile() {
         try {
             File file = new File(getName() + ".txt");
             FileReader fileReader = new FileReader(file);
@@ -48,19 +48,9 @@ public class Customer {
                 line = bufferedReader.readLine();
             }
             bufferedReader.close();
+            return true;
         } catch (Exception exception) {
-            System.out.println("An error has occurred");
-        }
-    }
-
-    public void makeFileIfNotFound() {
-        try {
-            File file = new File(getName() + ".txt");
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-        } catch (Exception exception) {
-            System.out.println("An errror has occurred");
+            return false;
         }
     }
 
@@ -84,7 +74,7 @@ public class Customer {
         }
     }
 
-    public void remakeFileFromCustomer() {
+    public boolean remakeFileFromCustomer() {
         try {
             File file = new File(getName() + ".txt");
             if (!file.exists()) {
@@ -122,8 +112,51 @@ public class Customer {
             add = add.trim();
             printWriter.println(add);
             printWriter.close();
+            return true;
         } catch (Exception exception) {
-            System.out.println("An error has occurred.");
+            return false;
+        }
+    }
+
+    public void removeFromWaitlistAtTime(int year,int month , int day , int hour, String storeName) {
+        for (Session session:waitingRequest) {
+            if (session.getYear() == year &&
+                    session.getMonth() == month &&
+                    session.getDay() == day &&
+                    session.getHour() == hour &&
+                    session.getStore().equals(storeName)){
+                waitingRequest.remove(session);
+            }
+        }
+    }
+
+    public void removeFromApprovedlistAtTime(int year,int month , int day , int hour, String storeName) {
+        for (Session session:approvedRequest) {
+            if (session.getYear() == year &&
+                    session.getMonth() == month &&
+                    session.getDay() == day &&
+                    session.getHour() == hour &&
+                    session.getStore().equals(storeName)){
+                approvedRequest.remove(session);
+            }
+        }
+    }
+
+    public void approveAppointmentAtTime(int year , int month, int day , int hour ,
+                                         String storeName) {
+        try {
+            for (Session session : waitingRequest) {
+                if (session.getYear() == year &&
+                        session.getMonth() == month &&
+                        session.getDay() == day &&
+                        session.getHour() == hour &&
+                        session.getStore().equals(storeName)) {
+                    waitingRequest.remove(session);
+                    approvedRequest.add(session);
+                }
+            }
+        } catch (Exception exception) {
+            return;
         }
     }
 }
