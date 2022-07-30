a448a49ae8e8d8e8f50494887a08bbf202045302
Aidan-Davis <98002205+Aidan-Davis@users.noreply.github.com>
2022-07-30 15:35:52 -0400

    Add files via upload

---
+++ GUITEST.java
@@ -0,0 +1,48 @@
+public class GUITEST {
+    public GUITEST() {
+    }
+
+    public void SellerMenuTest() {
+        Seller seller = new Seller("Bob");
+        Store store = new Store("bobMath" , "Bob");
+        Store store1 = new Store("bobReading" ,"Bob");
+        store.makeFileFromStore();
+        store1.makeFileFromStore();
+        seller.makeFileFromSeller();
+        seller.getStores().add(store);
+        seller.getStores().add(store1);
+        SellerMenuGUI sellerMenuGUI = new SellerMenuGUI(seller);
+        sellerMenuGUI.playGUI();
+    }
+
+    public void CustomerMenuGUITest() {
+        Customer customer = new Customer("Tom");
+        new CustomerMenuGUI(customer).playGUI();
+    }
+
+    public void NewAppointmentRequestTest() {
+        Seller seller = new Seller("Bob");
+        Store store = new Store("bobMath" , "Bob");
+        Store store1 = new Store("bobReading" ,"Bob");
+        store.makeFileFromStore();
+        store1.makeFileFromStore();
+        seller.makeFileFromSeller();
+        seller.getStores().add(store);
+        seller.getStores().add(store1);
+        Customer customer = new Customer("Tom");
+        new NewAppointmentRequest(customer ,store).playGUI();
+    }
+
+    public void CustomerSelectStoreTest() {
+        Customer customer = new Customer("Tom");
+        new CustomerSelectStoreGUI(customer).playGUI();
+    }
+
+    public void MainMenuTest() {
+        new MainMenuGUI().playGUI();
+    }
+    public static void main(String[] args) {
+        GUITEST guitest = new GUITEST();
+        guitest.CustomerMenuGUITest();
+    }
+}
