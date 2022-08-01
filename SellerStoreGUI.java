dcca5e0b0bb1e4992c65ab70360d97557aaba127
jmalex0106 <42811641+jmalex0106@users.noreply.github.com>
2022-08-01 09:30:20 -0400

    Update SellerStoreGUI.java

--- SellerStoreGUI.java
+++ SellerStoreGUI.java
@@ -7,11 +7,12 @@ import java.awt.event.ActionListener;
  * TODO ADD DESCRIPTIVE JAVADOCS
  *
  * @author Moxiao Li, Junmo Kim, Aidan Davis Lab 03 Group 08
+ *
  * @version date
+ *
  */
 
 public class SellerStoreGUI implements Runnable {
-    private static final String[] APP_LIST = {"Select Appointment"};
 
     private Seller seller;
 
@@ -28,7 +29,7 @@ public class SellerStoreGUI implements Runnable {
 
     @Override
     public void run() {
-        JFrame frame = new JFrame("Welcome to Tutoring Service");
+        JFrame frame = new JFrame("Welcome to " + store.getName());
 
         Container content = frame.getContentPane();
 
@@ -37,11 +38,30 @@ public class SellerStoreGUI implements Runnable {
         // Menu Panel
         JPanel panel1 = new JPanel(new GridLayout(2, 2)); // 2x2
 
-        JComboBox<String> appointmentDropDown = new JComboBox<>(APP_LIST);
+        String[] appList = new String[this.store.getSessions().size()];
+        for (int i = 0; i < this.store.getSessions().size(); i++) {
+            appList[i] = (this.store.getSessions().get(i).getMonth() + 1) +
+                    "/" + this.store.getSessions().get(i).getDay() +
+                    " " + this.store.getSessions().get(i).getYear() +
+                    " at " + this.store.getSessions().get(i).getHour() +
+                    ":00";
+        }
+        if (this.store.getSessions().size() == 0 ) {
+            appList = new String[]{"This store has no waiting or approved customers"};
+        }
+        JComboBox<String> appointmentDropDown = new JComboBox<>(appList);
         panel1.add(appointmentDropDown); // (1,1)
 
         JButton appointmentConfirm = new JButton("Confirm");
-//        appointmentConfirm.addActionListener(new );
+        appointmentConfirm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int appointmentSelected = appointmentDropDown.getSelectedIndex();
+                Session session = store.getSessions().get(appointmentSelected);
+                frame.dispose();
+                new SessionGUI(seller , store , session).playGUI();
+            }
+        });
         panel1.add(appointmentConfirm); // (1,2)
 
         JButton backToTutor = new JButton("Back to Tutor");
