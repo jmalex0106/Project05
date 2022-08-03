7c8b4c3cbe5943e8810dd5930c70d56c43c28be5
jmalex0106 <42811641+jmalex0106@users.noreply.github.com>
2022-08-02 20:09:22 -0400

    Delete TestMain.java

--- TestMain.java
+++
@@ -1,9 +0,0 @@
-import com.sun.source.doctree.SeeTree;
-
-public class TestMain {
-    public static void main(String[] args) {
-        Seller a = new Seller("Aidan");
-        a.remakeSellerFromFile();
-        System.out.println(a.getStores().size());
-    }
-}
